4af9e0eff47e48ddeafc0cb9e765fb499636f9c5
Ben Lovy <ben@deciduously.com>
2020-03-30 17:38:52 -0400

    Stage nosy

---
+++ Week11/nosy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# a
+echo -n "Please enter your first name: "
+read first
+echo -n "Please enter your middle name: "
+read middle
+echo -n "Please enter your last name: "
+read last
+echo "Hello, $first!"
